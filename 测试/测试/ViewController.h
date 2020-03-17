3adf5763164e5ea0d3b35b02f9686333e1c8e929
王战胜 <827940417@qq.com>
2020-03-17 19:51:20 +0800

    初始化

---
+++ 测试/测试/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  测试
+//
+//  Created by 王战胜 on 2020/3/17.
+//  Copyright © 2020年 SinosoftTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 测试/测试/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  测试
+//
+//  Created by 王战胜 on 2020/3/17.
+//  Copyright © 2020年 SinosoftTech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
